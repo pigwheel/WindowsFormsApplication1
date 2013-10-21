9d6ee33afcf2a4f89e093f37fd5c25738a23f5f5
singpeng <peng_enjoy@hotmail.com>
2013-10-21 14:05:31 +0700

    Home_Work
    แบบสอบถาม การบ้านครั้งที่1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.dpt2 = new System.Windows.Forms.RadioButton();
+            this.dpt1 = new System.Windows.Forms.RadioButton();
+            this.cmdCheck = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.cpt3 = new System.Windows.Forms.RadioButton();
+            this.cpt2 = new System.Windows.Forms.RadioButton();
+            this.cpt1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.apt1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.bpt1 = new System.Windows.Forms.RadioButton();
+            this.bpt2 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.groupBox6.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.dpt2);
+            this.groupBox6.Controls.Add(this.dpt1);
+            this.groupBox6.Location = new System.Drawing.Point(12, 264);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(428, 72);
+            this.groupBox6.TabIndex = 43;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "4.แต่งหรือไม่";
+            // 
+            // dpt2
+            // 
+            this.dpt2.AutoSize = true;
+            this.dpt2.Location = new System.Drawing.Point(126, 33);
+            this.dpt2.Name = "dpt2";
+            this.dpt2.Size = new System.Drawing.Size(58, 17);
+            this.dpt2.TabIndex = 21;
+            this.dpt2.TabStop = true;
+            this.dpt2.Text = "ไม่แต่ง";
+            this.dpt2.UseVisualStyleBackColor = true;
+            // 
+            // dpt1
+            // 
+            this.dpt1.AutoSize = true;
+            this.dpt1.Location = new System.Drawing.Point(5, 33);
+            this.dpt1.Name = "dpt1";
+            this.dpt1.Size = new System.Drawing.Size(45, 17);
+            this.dpt1.TabIndex = 20;
+            this.dpt1.TabStop = true;
+            this.dpt1.Text = "แต่ง";
+            this.dpt1.UseVisualStyleBackColor = true;
+            // 
+            // cmdCheck
+            // 
+            this.cmdCheck.Location = new System.Drawing.Point(152, 458);
+            this.cmdCheck.Name = "cmdCheck";
+            this.cmdCheck.Size = new System.Drawing.Size(100, 28);
+            this.cmdCheck.TabIndex = 39;
+            this.cmdCheck.Text = "เรียบร้อย";
+            this.cmdCheck.UseVisualStyleBackColor = true;
+            this.cmdCheck.Click += new System.EventHandler(this.cmdCheck_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.cpt3);
+            this.groupBox5.Controls.Add(this.cpt2);
+            this.groupBox5.Controls.Add(this.cpt1);
+            this.groupBox5.Location = new System.Drawing.Point(12, 198);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(428, 60);
+            this.groupBox5.TabIndex = 42;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "3.รถแบบไหน";
+            // 
+            // cpt3
+            // 
+            this.cpt3.AutoSize = true;
+            this.cpt3.Location = new System.Drawing.Point(230, 28);
+            this.cpt3.Name = "cpt3";
+            this.cpt3.Size = new System.Drawing.Size(55, 17);
+            this.cpt3.TabIndex = 19;
+            this.cpt3.TabStop = true;
+            this.cpt3.Text = "รถเก๋ง";
+            this.cpt3.UseVisualStyleBackColor = true;
+            // 
+            // cpt2
+            // 
+            this.cpt2.AutoSize = true;
+            this.cpt2.Location = new System.Drawing.Point(125, 28);
+            this.cpt2.Name = "cpt2";
+            this.cpt2.Size = new System.Drawing.Size(66, 17);
+            this.cpt2.TabIndex = 18;
+            this.cpt2.TabStop = true;
+            this.cpt2.Text = "รถปิคอัพ";
+            this.cpt2.UseVisualStyleBackColor = true;
+            // 
+            // cpt1
+            // 
+            this.cpt1.AutoSize = true;
+            this.cpt1.Location = new System.Drawing.Point(6, 28);
+            this.cpt1.Name = "cpt1";
+            this.cpt1.Size = new System.Drawing.Size(83, 17);
+            this.cpt1.TabIndex = 17;
+            this.cpt1.TabStop = true;
+            this.cpt1.Text = "รถครอบครัว";
+            this.cpt1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "แบบสอบถามการเลือกซื้อรถยนต์";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.apt1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 62);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(428, 45);
+            this.groupBox3.TabIndex = 40;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "1.คุณชอบรถยี่ห้อใด";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 22);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "โตโยต้า";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // apt1
+            // 
+            this.apt1.AutoSize = true;
+            this.apt1.Location = new System.Drawing.Point(6, 21);
+            this.apt1.Name = "apt1";
+            this.apt1.Size = new System.Drawing.Size(65, 17);
+            this.apt1.TabIndex = 7;
+            this.apt1.Text = "ฮอนด้าฃ";
+            this.apt1.UseVisualStyleBackColor = true;
+            this.apt1.CheckedChanged += new System.EventHandler(this.apt1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(236, 29);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "น้ำเงิน";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(160, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "แดง";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // bpt1
+            // 
+            this.bpt1.AutoSize = true;
+            this.bpt1.Location = new System.Drawing.Point(6, 29);
+            this.bpt1.Name = "bpt1";
+            this.bpt1.Size = new System.Drawing.Size(42, 17);
+            this.bpt1.TabIndex = 12;
+            this.bpt1.Text = "ขาว";
+            this.bpt1.UseVisualStyleBackColor = true;
+            // 
+            // bpt2
+            // 
+            this.bpt2.AutoSize = true;
+            this.bpt2.Location = new System.Drawing.Point(81, 29);
+            this.bpt2.Name = "bpt2";
+            this.bpt2.Size = new System.Drawing.Size(37, 17);
+            this.bpt2.TabIndex = 13;
+            this.bpt2.TabStop = true;
+            this.bpt2.Text = "ดำ";
+            this.bpt2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton7);
+            this.groupBox4.Controls.Add(this.radioButton3);
+            this.groupBox4.Controls.Add(this.radioButton1);
+            this.groupBox4.Controls.Add(this.bpt1);
+            this.groupBox4.Controls.Add(this.bpt2);
+            this.groupBox4.Location = new System.Drawing.Point(12, 113);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(428, 68);
+            this.groupBox4.TabIndex = 41;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "2.สีอะไร";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(166, 21);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(74, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.Text = "เชฟโรเลต";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(255, 22);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(55, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.Text = "มาสด้า";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(330, 22);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 17);
+            this.radioButton6.TabIndex = 45;
+            this.radioButton6.Text = "ฟอร์ด";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(310, 29);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(59, 17);
+            this.radioButton7.TabIndex = 16;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "บรอนด์";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton8);
+            this.groupBox1.Controls.Add(this.radioButton9);
+            this.groupBox1.Location = new System.Drawing.Point(12, 352);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 72);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "5.เบาะอะไร";
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(126, 33);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(63, 17);
+            this.radioButton8.TabIndex = 21;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "กำมะหยี่";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(5, 33);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(45, 17);
+            this.radioButton9.TabIndex = 20;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "หนัง";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 514);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.cmdCheck);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.RadioButton dpt2;
+        private System.Windows.Forms.RadioButton dpt1;
+        private System.Windows.Forms.Button cmdCheck;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RadioButton cpt3;
+        private System.Windows.Forms.RadioButton cpt2;
+        private System.Windows.Forms.RadioButton cpt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton apt1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton bpt1;
+        private System.Windows.Forms.RadioButton bpt2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCheck_Click(object sender, EventArgs e)
+        {
+            string str = "แบบสอบถามการเลือกรถยนต์ :" + Environment.NewLine;
+
+
+            str = str + "1.คุณชอบรถยี่ห้อใด ===";
+            if (apt1.Checked == true)
+            {
+                str = str + "ฮอนด้า" + Environment.NewLine;
+
+
+            }
+            if (radioButton2.Checked == true)
+            {
+                str = str + "โตโยต้า" + Environment.NewLine;
+
+            }
+            if (radioButton4.Checked == true)
+            {
+                str = str + "เชฟโลเลต" + Environment.NewLine;
+
+            }
+            if (radioButton5.Checked == true)
+            {
+                str = str + "มาสด้า" + Environment.NewLine;
+
+            }
+            if (radioButton6.Checked == true)
+            {
+                str = str + "ฟอร์ด" + Environment.NewLine;
+
+            }
+            str = str + "2.สีอะไร ===";
+            if (bpt1.Checked == true)
+            {
+                str = str + "ขาว" + Environment.NewLine;
+            }
+            if (bpt2.Checked == true)
+            {
+                str = str + "ดำ" + Environment.NewLine;
+            }
+            if (radioButton1.Checked == true)
+            {
+                str = str + "แดง" + Environment.NewLine;
+
+            }
+            if (radioButton3.Checked == true)
+            {
+                str = str + "น้ำเงิน" + Environment.NewLine;
+
+            }
+            if (radioButton7.Checked == true)
+            {
+                str = str + "บรอนด์" + Environment.NewLine;
+
+            }
+            str = str + "3.รถแบบไหน ===";
+            if (cpt1.Checked == true)
+            {
+                str = str + "รถครอบครัว" + Environment.NewLine;
+            }
+            if (cpt2.Checked == true)
+            {
+                str = str + "รถปิคอัพ" + Environment.NewLine;
+            }
+            if (cpt3.Checked == true)
+            {
+                str = str + "รถเก๋ง" + Environment.NewLine;
+
+            }
+            
+            str = str + "4.แต่งหรือไม่ ===";
+            if (dpt1.Checked == true)
+            {
+                str = str + "แต่ง" + Environment.NewLine;
+            }
+            if (dpt2.Checked == true)
+            {
+                str = str + "ไม่แต่ง" + Environment.NewLine;
+            }
+            str = str + "5.เบาะอะไร ===";
+            if (radioButton9.Checked == true)
+                str = str + "หนัง" + Environment.NewLine;
+            {
+                if (radioButton8.Checked == true)
+                    str = str + "กำมะหยี่" + Environment.NewLine;
+                }
+            
+            
+            MessageBox.Show(str, "รายงานผล");
+        }
+
+        private void optMale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apt2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apt1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
